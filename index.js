89abb0c6e31a2e284cb37b086918e9eb4410f56e
Kash Reid-Bashir <kash@reid-bashir.com>
2019-02-03 10:05:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+return "Happy Holidays"
+}
+// function which takes in a parameter.
+function happyHolidaysTo(Name) {
+return "Happy Holidays, ${Name}"
+}
+// function which takes in multiple parameters
+function happyHolidaysTo(Holiday, Name) {
+return "Happy Holidays, ${Holiday}, ${Name}"
+}
+// function which takes in multiple parameters
+function happyHolidaysTo(Holiday, Days) {
+return "Happy holidays, congratulations you have ${Days} until you leave for ${Holiday}"
+}
