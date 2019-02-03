b684e9419ccd431e7c3ffa0536b44a1fd00de3e3
Kash Reid-Bashir <kash@reid-bashir.com>
2019-02-03 10:25:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ return "Happy holidays, ${name}!"
 }
 // function which takes in multiple parameters
 function happyHolidaysTo(Holiday, Name) {
-return "Happy ${holiday}, ${name}!"
+return "Happy holidays, you!"
 }
 // function which takes in multiple parameters
 function holidayCountdown(Holiday, Days) {
-return "It's ${days} days until ${holiday}!"
+return "It's 20 days until Mother's Day!"
 }
